aa8d53e89aa852886dd949093999f1176c78946f
Emmanuel Plumas <eplumas@free.fr>
2021-06-06 21:02:47 +0200

    Amélioration de la connexion et deconnexion
    Ajout de connexion et deconnexion en fonction de l'utilisateur
    Ajout du nom de l'utilisateur sur la page d'accueil

--- p12-JavaVegetAbleWEB/src/main/java/com/emmanuel/plumas/p12JavaVegetAbleWEB/controller/HomeController.java
+++ p12-JavaVegetAbleWEB/src/main/java/com/emmanuel/plumas/p12JavaVegetAbleWEB/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.emmanuel.plumas.p12JavaVegetAbleWEB.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class HomeController{
+public class HomeController extends CommonController{
 	
 	@GetMapping(value="/")
-	public String homePage() {
+	public String homePage(Model model) {
+		String userIdentifiant=getPrincipal();
+		model.addAttribute("principal", userIdentifiant);
 		return "homepage";
 	}
 	
