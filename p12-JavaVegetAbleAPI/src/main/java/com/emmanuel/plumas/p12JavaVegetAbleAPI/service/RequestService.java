c994fc54b638e3da412ac349872d8399957f8c1d
Emmanuel Plumas <eplumas@free.fr>
2021-06-26 17:53:22 +0200

    Optimisation de la requete de recherche des demandes reçues
    On ne charge maintenant que les provision de l'utilisateur et parmi
    celles-ci que les provisions qui ont une request.

--- p12-JavaVegetAbleAPI/src/main/java/com/emmanuel/plumas/p12JavaVegetAbleAPI/service/RequestService.java
+++ p12-JavaVegetAbleAPI/src/main/java/com/emmanuel/plumas/p12JavaVegetAbleAPI/service/RequestService.java
@@ -7,45 +7,49 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.emmanuel.plumas.p12JavaVegetAbleAPI.model.ProvisionEntity;
 import com.emmanuel.plumas.p12JavaVegetAbleAPI.model.RequestEntity;
 import com.emmanuel.plumas.p12JavaVegetAbleAPI.repositories.IRequestRepository;
 
 @Service
 public class RequestService {
-	
+
 	@Autowired
 	private IRequestRepository requestRepository;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private ProvisionService provisionService;
-	
-	public List<RequestEntity> getSendedRequestsByUserIdentifiant(String userIdentifiant){
+
+	public List<RequestEntity> getSendedRequestsByUserIdentifiant(String userIdentifiant) {
 		return requestRepository.findByUserEntityUserIdentifiant(userIdentifiant);
 	}
-	
-	public List<RequestEntity> getReceivedRequestByUserIdentifiant(String userIdentifiant){
-		List<RequestEntity> requestEntities = (List<RequestEntity>) requestRepository.findAll();
+
+	public List<RequestEntity> getReceivedRequestByUserIdentifiant(String userIdentifiant) {
+		List<ProvisionEntity> provisionEntities = provisionService.getProvisionsByUserIdentifiant(userIdentifiant);
+		List<RequestEntity> receivedRequestEntities= new ArrayList<RequestEntity>();
 		
-		List<RequestEntity> receivedRequestEntities=new ArrayList<RequestEntity>();
-		for(RequestEntity requestEntity : requestEntities) {
-			if(requestEntity.getProvisionEntity().getUserEntity().getUserIdentifiant().equals(userIdentifiant)) {
-				receivedRequestEntities.add(requestEntity);
+		for (ProvisionEntity provisionEntity : provisionEntities) {
+			List<RequestEntity> requestEntities = requestRepository
+					.findByProvisionEntityProvisionId(provisionEntity.getProvisionId());
+			for (RequestEntity requeteEntity : requestEntities) {
+				receivedRequestEntities.add(requeteEntity);
 			}
 		}
 		return receivedRequestEntities;
 	}
 
 	public void createRequestEntity(RequestEntity requestEntity) {
-		requestEntity.setProvisionEntity(provisionService.getProvisionEntityByProvisionId(requestEntity.getProvisionEntity().getProvisionId()));
+		requestEntity.setProvisionEntity(
+				provisionService.getProvisionEntityByProvisionId(requestEntity.getProvisionEntity().getProvisionId()));
 		requestEntity.setUserEntity(userService.getUserById(requestEntity.getUserEntity().getUserId()));
-		//Status par défaut à la création de la demande
+		// Status par défaut à la création de la demande
 		requestEntity.setRequestStatus("En attente d'acceptation");
 		Date date = new Date();
 		requestEntity.setRequestDate(date);
-		requestRepository.save(requestEntity);		
+		requestRepository.save(requestEntity);
 	}
-	
+
 }
